28a37ade570bb541a9387c31186b9a86b74377d6
witwitchayakarn <witwitchayakarn@gmail.com>
2020-12-18 20:21:57 +0700

    No need DispatchQueue in RNBlockstackSdk.swift as React Native already creates a separate GCD queue

--- BlockstackSDK/ios/RNBlockstackSdk/RNBlockstackSdk.swift
+++ BlockstackSDK/ios/RNBlockstackSdk/RNBlockstackSdk.swift
@@ -11,7 +11,7 @@ class RNBlockstackSdk: NSObject {
     private var isLoaded = false
   
     @objc static func requiresMainQueueSetup() -> Bool {
-        return true
+        return false
     }
 
     @objc public func hasSession(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
@@ -24,154 +24,142 @@ class RNBlockstackSdk: NSObject {
         resolve(["loaded": self.isLoaded])
     }
 
-    @objc public func isUserSignedIn(_ resolve: @escaping RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            resolve(["signedIn": Blockstack.shared.isUserSignedIn()])
-        }
+    @objc public func isUserSignedIn(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+        resolve(["signedIn": Blockstack.shared.isUserSignedIn()])
     }
-    
+
     @objc public func signIn(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            guard let config = self.config,
-                  let appDomainString = config["appDomain"] as? String,
-                  let appDomain = URL(string: appDomainString),
-                  let redirectUrlString = config["redirectUrl"] as? String,
-                  let redirectURL = URL(string: redirectUrlString, relativeTo: appDomain) else {
-                reject(self.defaultErrorCode, "Invalid Blockstack session config data", nil)
+
+        guard let config = self.config,
+              let appDomainString = config["appDomain"] as? String,
+              let appDomain = URL(string: appDomainString),
+              let redirectUrlString = config["redirectUrl"] as? String,
+              let redirectURL = URL(string: redirectUrlString, relativeTo: appDomain) else {
+            reject(self.defaultErrorCode, "Invalid Blockstack session config data", nil)
+            return
+        }
+
+        var manifestURI: URL?
+        if let manifestPath = config["manifestUrl"] as? String {
+            manifestURI = URL(string: manifestPath)
+        }
+
+        let scopes = (config["scopes"] as? [String] ?? ["store_write"]).compactMap { AuthScope.fromString($0) }
+
+        Blockstack.shared.signIn(redirectURI: redirectURL, appDomain: appDomain, manifestURI: manifestURI, scopes: scopes) { authResult in
+            switch authResult {
+            case let .success(userData: userData):
+                let data = [
+                    "result": "success",
+                    "user_data": userData.dictionary ?? [],
+                    "decentralizedID": userData.username ?? "",
+                    "loaded": true,
+                ] as [String : Any]
+                resolve(data)
                 return
-            }
-
-            var manifestURI: URL?
-            if let manifestPath = config["manifestUrl"] as? String {
-                manifestURI = URL(string: manifestPath)
-            }
-
-            let scopes = (config["scopes"] as? [String] ?? ["store_write"]).compactMap { AuthScope.fromString($0) }
-
-            Blockstack.shared.signIn(redirectURI: redirectURL, appDomain: appDomain, manifestURI: manifestURI, scopes: scopes) { authResult in
-                switch authResult {
-                case let .success(userData: userData):
-                    let data = [
-                      "result": "success",
-                      "user_data": userData.dictionary ?? [],
-                      "decentralizedID": userData.username ?? "",
-                      "loaded": true,
-                    ] as [String : Any]
-                    resolve(data)
-                    return
-                case let .failed(err):
-                    let errMsg = err?.localizedDescription ?? "Error"
-                    reject(self.defaultErrorCode, errMsg, err)
-                    return
-                case .cancelled:
-                    reject(self.defaultErrorCode, "signIn cancelled", nil)
-                    return
-                }
+            case let .failed(err):
+                let errMsg = err?.localizedDescription ?? "Error"
+                reject(self.defaultErrorCode, errMsg, err)
+                return
+            case .cancelled:
+                reject(self.defaultErrorCode, "signIn cancelled", nil)
+                return
             }
         }
     }
 
-    @objc public func signUserOut(_ resolve: @escaping RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            Blockstack.shared.signUserOut()
-            resolve(["signedOut": true])
-        }
+    @objc public func signUserOut(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+        Blockstack.shared.signUserOut()
+        resolve(["signedOut": true])
     }
     
-    @objc public func loadUserData(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            let userData = Blockstack.shared.loadUserData()
-            guard userData != nil else {
-                reject(self.defaultErrorCode, "loadUserData returns nil", nil)
-                return
-            }
+    @objc public func loadUserData(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
 
-            resolve(userData.dictionary ?? [])
+        let userData = Blockstack.shared.loadUserData()
+        guard userData != nil else {
+            reject(self.defaultErrorCode, "loadUserData returns nil", nil)
+            return
         }
+
+        resolve(userData.dictionary ?? [])
     }
     
     @objc public func putFile(_ fileName: String!, content: String!, options: NSDictionary?, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .utility).async {
-            let encrypt = options?["encrypt"] as? Bool ?? true
-            let escapedFileName = fileName.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
-            Blockstack.shared.putFile(to: escapedFileName, text: content, encrypt: encrypt) { result, error in
-                guard let fileUrl = result, error == nil else {
-                    reject(self.defaultErrorCode, "putFile Error", error)
-                    return
-                }
-                resolve(["fileUrl": fileUrl])
+        let encrypt = options?["encrypt"] as? Bool ?? true
+        let escapedFileName = fileName.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+        Blockstack.shared.putFile(to: escapedFileName, text: content, encrypt: encrypt) { result, error in
+            guard let fileUrl = result, error == nil else {
+                reject(self.defaultErrorCode, "putFile Error", error)
+                return
             }
+            resolve(["fileUrl": fileUrl])
         }
     }
     
     @objc public func getFile(_ path: String!, options: NSDictionary?, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .utility).async {
-            // TODO: Support multiplayer
-            // let username = options?["username"] as? String
-            let decrypt = options?["decrypt"] as? Bool ?? true
-            let escapedPath = path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
-            Blockstack.shared.getFile(at: escapedPath, decrypt: decrypt) {
-                value, error in
+        // TODO: Support multiplayer
+        // let username = options?["username"] as? String
+        let decrypt = options?["decrypt"] as? Bool ?? true
+        let escapedPath = path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+        Blockstack.shared.getFile(at: escapedPath, decrypt: decrypt) {
+            value, error in
 
-                guard error == nil else {
-                    reject(self.defaultErrorCode, "getFile Error", error)
+            guard error == nil else {
+                reject(self.defaultErrorCode, "getFile Error", error)
+                return
+            }
+            
+            if decrypt {
+                guard let decryptedValue = value as? DecryptedValue else {
+                    reject(self.defaultErrorCode, "In getFile, options decrypt is true but value is not DecryptedValue.", error)
+                    return
+                }
+
+                if decryptedValue.isString {
+                    resolve(["fileContents": decryptedValue.plainText])
                     return
                 }
                 
-                if decrypt {
-                    guard let decryptedValue = value as? DecryptedValue else {
-                        reject(self.defaultErrorCode, "In getFile, options decrypt is true but value is not DecryptedValue.", error)
-                        return
-                    }
-
-                    if decryptedValue.isString {
-                        resolve(["fileContents": decryptedValue.plainText])
-                        return
-                    }
-                    
-                    resolve(["fileContentsEncoded": decryptedValue.bytes?.toBase64()])
-                    return
-                }
-
-                resolve(["fileContents": value])
+                resolve(["fileContentsEncoded": decryptedValue.bytes?.toBase64()])
+                return
             }
+
+            resolve(["fileContents": value])
         }
     }
 
     @objc public func deleteFile(_ path: String!, options: NSDictionary?, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .utility).async {
-            let wasSigned = options?["wasSigned"] as? Bool ?? false
-            let escapedPath = path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
-            Blockstack.shared.deleteFile(at: escapedPath, wasSigned: wasSigned) {
-                error in
 
-                guard error == nil else {
-                    reject(self.defaultErrorCode, "deleteFile Error", error)
-                    return
-                }
+        let wasSigned = options?["wasSigned"] as? Bool ?? false
+        let escapedPath = path.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)!
+        Blockstack.shared.deleteFile(at: escapedPath, wasSigned: wasSigned) {
+            error in
 
-                resolve(["deleted": true])
+            guard error == nil else {
+                reject(self.defaultErrorCode, "deleteFile Error", error)
+                return
             }
+
+            resolve(["deleted": true])
         }
     }
 
     @objc public func listFiles(_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        DispatchQueue.global(qos: .utility).async {
-            // list all files and return to JS just once
-            var files = [String]()
-            Blockstack.shared.listFiles(callback: {
-                files.append($0)
-                return true
-            }, completion: { fileCount, error in
 
-                guard error == nil else {
-                    reject(self.defaultErrorCode, "listFiles Error", error)
-                    return
-                }
+        // list all files and return to JS just once
+        var files = [String]()
+        Blockstack.shared.listFiles(callback: {
+            files.append($0)
+            return true
+        }, completion: { fileCount, error in
 
-                resolve(["files": files, "fileCount": fileCount])
-            })
-        }
+            guard error == nil else {
+                reject(self.defaultErrorCode, "listFiles Error", error)
+                return
+            }
+
+            resolve(["files": files, "fileCount": fileCount])
+        })
     }
 }
 
